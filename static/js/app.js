f726964f465d861b89371690104de1aa7cbbc2de
Leo-SLC <leoantinozzi@Leos-MacBook-Pro.local>
2023-09-13 15:31:47 -0600

    Pushing Code

--- static/js/app.js
+++ static/js/app.js
@@ -23,16 +23,49 @@ const optionChanged = () => {
         .text(`${key.toUpperCase()}: ${val}`);
     });
 
+    const{otu_ids, otu_labels, sample_values} = sample;
+
     var data = [
         {
-          x: ['giraffes', 'orangutans', 'monkeys'],
-          y: [20, 14, 23],
+          x: sample_values.slice(0, 10).reverse(),
+          y: otu_ids.slice(0, 10).reverse().map(y => `OTU ${y}`),
+          text: otu_labels.slice(0,10).reverse(),
           type: 'bar',
           orientation:"h"
         }
       ];
       
-      Plotly.newPlot('bar', data);    
+      Plotly.newPlot('bar', data); 
+      
+      var trace1 = {
+        x: otu_ids,
+        y: sample_values,
+        text: otu_labels,
+        mode: 'markers',
+        marker: {
+          size: sample_values,
+          color: otu_ids,
+          colorscale: "Earth"
+        }
+      };
+      
+      var data = [trace1];
+      
+      Plotly.newPlot('bubble', data);
+
+      var data = [
+        {
+          domain: { x: [0, 1], y: [0, 1] },
+          value: meta.wfreq,
+          title: { text: "<b>Belly Button Washing Frequency</b> <br> Scrubs per week"},
+          type: "indicator",
+          mode: "gauge+number",
+          delta: { reference: 400 },
+          gauge: { axis: { range: [null, 9] } }
+        }
+      ];
+      Plotly.newPlot('gauge', data);
 
   });
 };
+
